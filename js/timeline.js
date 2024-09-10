3d2e9aaf7a45fbca0fa94f7697a40e23fdd9d335
Oumnia El Mokretar <oumnia.elmokretar@railconcept.fr>
2024-09-10 18:33:22 +0200

    wip adjust timeline and correlate with elements

--- js/timeline.js
+++ js/timeline.js
@@ -23,39 +23,64 @@ function setupScroll() {
     const timeline = document.querySelector(".js-timeline");
     const timelineLine = document.querySelector(".js-timeline_line");
     const timelineLineProgress = document.querySelector(".js-timeline_line-progress");
-    const timeLineItems = document.querySelectorAll(".js-timeline_item");
 
+    const timeLineItems = Array.from(document.querySelectorAll(".js-timeline-items-container > *"));
+    const bookmarks = Array.from(document.querySelectorAll(".js-timeline-bookmark-container > *"));
+
+    const bookmarksWithItems = bookmarks.map(
+        (bookmark, i) => ({
+            bookmark: bookmark,
+            item: timeLineItems[i]
+        }));
+
+    console.log(bookmarksWithItems);
+
+    
     let oldWindowScrollTop = -1;
     let agFlag = false;
     let windowScrollTop = 0;
+
+    function refreshBookmarksPositions() {
+        const timelineBox = timeline.getBoundingClientRect();
+        const offsetTimelineTopRelativeToDocument = windowScrollTop + timelineBox.top;
+
+        bookmarksWithItems.forEach(itemWithBookmark => {
+            if (itemWithBookmark.bookmark && itemWithBookmark.item) {
+                const itemBox = itemWithBookmark.item.getBoundingClientRect();
+                const bookmarkTop = itemBox.top - offsetTimelineTopRelativeToDocument + windowScrollTop;
+                itemWithBookmark.bookmark.style.top = bookmarkTop + "px";
+            }
+        });
+
+    }
 
     function fnUpdateProgress() {
         const timelineBox = timeline.getBoundingClientRect();
 
         // header / navbar height
         const offsetTimelineTopRelativeToDocument = windowScrollTop + timelineBox.top;
-        // footer height
-        const offsetTimelineBottomRelativeToDocument = document.documentElement.scrollHeight - (timelineBox.bottom + windowScrollTop);
 
         // if you want to increase of decrease the progress part of the timeline adjust this ratio
         const viewportHeightProgressLineRatio = 0.4;
         const baseLinePositionRelativeToViewport = viewportHeightProgressLineRatio * window.innerHeight;
         const progressBarHeightRelativeToTimeLine = baseLinePositionRelativeToViewport - offsetTimelineTopRelativeToDocument + windowScrollTop;
 
-        timelineLine.style.height = timeline.scrollHeight + offsetTimelineBottomRelativeToDocument - ((1 - viewportHeightProgressLineRatio) * window.innerHeight) + "px";
+        timelineLine.style.height = timeline.scrollHeight + "px";
         timelineLineProgress.style.height = progressBarHeightRelativeToTimeLine + "px";
 
         // if you want to make elements appear sooner or later adjust this ratio
         const viewportHeightRatioForActiveTrigger = 0.4;
         
-        timeLineItems.forEach((el, i) => {
+        bookmarksWithItems.forEach((el, i) => {
             const minPositionToBeActive = window.innerHeight * viewportHeightRatioForActiveTrigger; 
-            const elementTop = el.getBoundingClientRect().top;
+            const elementTop = el.item.getBoundingClientRect().top;
 
             if (elementTop <= minPositionToBeActive) {
-                el.classList.add("active");
+                el.item.classList.add("active");
+                el.bookmark.classList.add("active");
             } else {
-                el.classList.remove("active");
+                el.item.classList.remove("active");
+                el.bookmark.classList.remove("active");
             }
         });
     }
@@ -66,6 +91,7 @@ function setupScroll() {
         if (oldWindowScrollTop !== windowScrollTop) {
             oldWindowScrollTop = windowScrollTop;
             fnUpdateProgress();
+            refreshBookmarksPositions();
         }
     }
     function refresh() {
@@ -80,6 +106,7 @@ function setupScroll() {
     window.addEventListener("resize", refresh);
 
     refresh();
+
 }
 
 function ready(fn) {
